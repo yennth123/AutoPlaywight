c0f61f1da256984c093180fa8d5fb92749c8b35c
yennth123 <127300979+yennth123@users.noreply.github.com>
2025-03-25 20:14:06 +0700

    abc

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,13 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('get started link3', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
